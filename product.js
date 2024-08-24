721c1e9ac21141028df6b847a2bc65080654a14d
sandiarbaa <sandiarbaa@gmail.com>
2024-08-24 19:02:28 +0700

    membuat custom method untuk model dari schema

--- product.js
+++ product.js
@@ -60,7 +60,32 @@ const productSchema = mongoose.Schema({
   },
 });
 
+productSchema.methods.outStock = function () {
+  // this di sini mengacu ke objeck schema nya
+  this.stock = 0;
+  this.availability.online = false;
+  this.availability.offline = false;
+  return this.save();
+};
+
 const Product = mongoose.model("Product", productSchema);
+
+// membuat method contohnya outStock, itu agar logic yg panjang/kompleks bisa di pisah
+// jadi memisahkan antara logic dengan proses eksekusi query nya saja.
+const changeStock = async (id) => {
+  // query
+  const foundProduct = await Product.findById(id);
+  // logic di ambil dari method outStock
+  await foundProduct.outStock(); // memanggil method outStock dari model, berdasarkan data yg dicari dengan id misal
+  console.log("Berhasil diubah");
+};
+
+changeStock("66c9b1f88500869fab50faff");
+
+// intinya kita bisa membuat instance method dari schema nya, kemudian di pakai di dalam model nya
+// jika model nya butuh perubahan data, jadi logic yg kompleks nya di instance method schema, dan query nya di modelnya
+// dalam hal ini membuat function changeStock untuk model nya
+// di dalam function changeStock itu ada query, yaitu mencari product berdasarkan id, lalu memanggil method outStock
 
 // const product = new Product({
 //   name: "Kemeja Flanel",
@@ -89,28 +114,29 @@ const Product = mongoose.model("Product", productSchema);
 
 // harus di tambahkan dulu options runValidators, supaya validasi yg di schema dijalankan
 // findOneAndUpdate | findByIdAndUpdate
-Product.findOneAndUpdate(
-  { name: "Kemeja Flanel" },
-  {
-    name: "Kemeja Flanel",
-    brand: "Hollister",
-    price: -150000,
-    color: "biru muda",
-    size: ["S", "M", "L"],
-    description:
-      "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
-    condition: "baru",
-    stock: -10,
-    availability: {
-      online: true,
-      offline: true,
-    },
-  },
-  { new: true, runValidators: true }
-)
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.error(err.errors.stock.properties.message);
-  });
+
+// Product.findOneAndUpdate(
+//   { name: "Kemeja Flanel" },
+//   {
+//     name: "Kemeja Flanel",
+//     brand: "Hollister",
+//     price: -150000,
+//     color: "biru muda",
+//     size: ["S", "M", "L"],
+//     description:
+//       "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
+//     condition: "baru",
+//     stock: -10,
+//     availability: {
+//       online: true,
+//       offline: true,
+//     },
+//   },
+//   { new: true, runValidators: true }
+// )
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.error(err.errors.stock.properties.message);
+//   });
